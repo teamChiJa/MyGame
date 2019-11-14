a751c82498f969f62ad7722018af382edd867e3c
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-14 14:54:57 +0900

    数字入力

--- src/mygame/Insert.java
+++ src/mygame/Insert.java
@@ -6,11 +6,32 @@ import java.util.Scanner;
 public class Insert {
 
     static String str;
+    static int nm;
     static Scanner scan = new Scanner(System.in);
 
     public static String insert() {
         str = scan.next();
         return str;
+    }
+
+    public static int insertNumber() {
+        for (;;) {
+
+            try {
+                str = scan.next();
+                nm = Integer.parseInt(str);
+            } catch (NumberFormatException e) {
+                System.out.println("▲入力エラー：数値を入力してください▲");
+                continue;
+            }
+            if (nm >= 100000) {
+                System.out.println("▲入力エラー：数値が上限を超えています▲");
+                continue;
+            }
+
+            return nm;
+
+        }
     }
 
 }
