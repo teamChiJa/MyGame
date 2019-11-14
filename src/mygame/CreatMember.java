221f3173ff07438ca764dd5f220f9e0d2635a2d2
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-11-14 14:04:17 +0900

    partyリスト追加

--- src/mygame/CreatMember.java
+++ src/mygame/CreatMember.java
@@ -6,6 +6,6 @@ public class CreatMember {
 
     ArrayList<Job> party = new ArrayList();
     public static void Creat(){
-        System.out.println("");
+        System.out.println("");//
     }
 }
