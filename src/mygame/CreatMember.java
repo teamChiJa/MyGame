2e52c24da8e8e6f6e1def57cf922d4d9cd989da5
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-11-14 14:34:21 +0900

    static追加

--- src/mygame/CreatMember.java
+++ src/mygame/CreatMember.java
@@ -1,11 +1,12 @@
 package mygame;
 
 import java.util.ArrayList;
-
+import static mygame.Insert.*;
+import static mygame.MemberManager.*;
 public class CreatMember {
 
-    ArrayList<Job> party = new ArrayList();
+    static ArrayList<Job> party = new ArrayList();
     public static void Creat(){
-        System.out.println("");//
+        System.out.println("");
     }
 }
