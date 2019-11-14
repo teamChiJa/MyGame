46f047cbf61704da0eb9b187723d9d9c2ba1b4f4
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-11-14 10:41:03 +0900

    初期

---
+++ src/mygame/MyGame.java
@@ -0,0 +1,16 @@
+package mygame;
+
+/**
+ *
+ * @author kbc19a17
+ */
+public class MyGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
