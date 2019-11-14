cbbad06c93d80d7cf99efd9e7cc274fb44e01ada
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-14 14:36:19 +0900

    MemberManagementSystem02

--- src/mygame/CreatMember.java
+++ src/mygame/CreatMember.java
@@ -3,7 +3,7 @@ package mygame;
 import java.util.ArrayList;
 import static mygame.Insert.*;
 import static mygame.MemberManager.*;
-public class CreatMember {
+public class CreatMember {//
 
     static ArrayList<Job> party = new ArrayList();
     public static void Creat(){
