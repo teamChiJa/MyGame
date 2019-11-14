76c29a982b3b0d0b75d31e7877051665df853604
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-14 14:38:52 +0900

    MemberManagementSystem03

--- src/mygame/MemberManager.java
+++ src/mygame/MemberManager.java
@@ -9,12 +9,12 @@ public class MemberManager {
     static Map<String, ArrayList<Job>> hmm = new HashMap<>();
     CreatMember cm = new CreatMember();
 
-    public void hmmPut(String groupName, ArrayList<Job> al) {
+    public static void hmmPut(String groupName, ArrayList<Job> al) {
         hmm.put(groupName, al);
         System.out.println(groupName + "　グループをセーブしました");
     }
 
-    public void hmmToString() {
+    public static void hmmToString() {
         System.out.println();
         for(String group : hmm.keySet()){
             System.out.println(  "グループ名 : " + group);
