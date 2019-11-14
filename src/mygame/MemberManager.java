870e6719ab3a3a5a8d614022eb8cf787a35dd7ea
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-14 11:06:15 +0900

    初期

---
+++ src/mygame/MemberManager.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+/**
+ *
+ * @author kbc19a21
+ */
+public class MemberManager {
+    
+}
