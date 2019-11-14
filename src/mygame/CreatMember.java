a8b94f8244502e4a2f1cca1c4bbe24dad9ba411c
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-11-14 14:03:01 +0900

    partyリスト追加

--- src/mygame/CreatMember.java
+++ src/mygame/CreatMember.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mygame;
 
-/**
- *
- * @author kbc19a21
- */
+import java.util.ArrayList;
+
 public class CreatMember {
-    
+
+    ArrayList<Job> party = new ArrayList();
+    public static void Creat(){
+        System.out.println("");
+    }
 }
