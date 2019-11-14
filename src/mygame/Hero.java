ec0d05f858e0af52122e84d7b09153cdcc18c6fe
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-14 12:20:30 +0900

    Heroクラス継承

--- src/mygame/Hero.java
+++ src/mygame/Hero.java
@@ -2,40 +2,16 @@ package mygame;
 
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Hero {
+public class Hero extends Job {
 
     private String name;
-    private int HP;
-    private int MP;
-    private int Attack;
-    private int Defence;
+    private int hp;
+    private int mp;
+    private int attack;
+    private int defence;
 
-    public Hero(String name) {
-        this.name = name;
-        HP = ThreadLocalRandom.current().nextInt(30, 40);
-        MP = ThreadLocalRandom.current().nextInt(10, 20);
-        Attack = 20;
-        Defence = 20;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHP() {
-        return HP;
-    }
-
-    public int getMP() {
-        return MP;
-    }
-
-    public int getAttack() {
-        return Attack;
-    }
-
-    public int getDefence() {
-        return Defence;
+    public Hero(String name, int hp, int mp, int attack, int defence) {
+        super(name, hp, mp, attack, defence);
     }
 
 }
