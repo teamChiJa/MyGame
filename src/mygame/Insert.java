75b9f95b04fb372e7298db03b20dc4ecc3bbc332
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-14 14:50:51 +0900

    MemberManagementSystem03

--- src/mygame/Insert.java
+++ src/mygame/Insert.java
@@ -1,14 +1,16 @@
-
 package mygame;
 
 import java.util.Scanner;
 
 //文字入力
 public class Insert {
+
     static String str;
     static Scanner scan = new Scanner(System.in);
-    public static String insert(){
+
+    public static String insert() {
         str = scan.next();
-        return str; 
+        return str;
     }
+
 }
