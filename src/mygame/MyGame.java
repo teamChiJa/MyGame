40dd567867690cd8cc89fd169b460c931a32c560
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-11-18 09:52:18 +0900

    テスト

--- src/mygame/MyGame.java
+++ src/mygame/MyGame.java
@@ -1,16 +1,11 @@
 package mygame;
+import static mygame.CreatMember.*;
+import static mygame.MemberManager.*;
 
-/**
- *
- * @author kbc19a17
- */
 public class MyGame {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        creat();
     }
-    
+
 }
