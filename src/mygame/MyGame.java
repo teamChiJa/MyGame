a80f277af7191bdfce4dff49d20916645247eb44
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-19 08:27:59 +0900

    mygame : update

--- src/mygame/MyGame.java
+++ src/mygame/MyGame.java
@@ -2,6 +2,7 @@ package mygame;
 
 import static mygame.CreatMember.*;
 import static mygame.MemberManager.*;
+import static mygame.Insert.*;
 import static mygame.PlayGame.*;
 
 public class MyGame {
@@ -9,6 +10,11 @@ public class MyGame {
     public static void main(String[] args) {
         for (;;) {
             game();
+            System.out.println("continueしますか？");
+            String ctn = foolProof();
+            if(ctn.equalsIgnoreCase("n")){
+                break;
+            }
         }
     }
 
