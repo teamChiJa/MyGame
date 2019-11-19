0aac4d51c0933836c4d0893b717953e1b84e7c62
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-11-19 11:55:15 +0900

    エラー修正

--- src/mygame/MemberManager.java
+++ src/mygame/MemberManager.java
@@ -9,32 +9,26 @@ public class MemberManager {
 
     static Map<String, ArrayList<Job>> hmm = new HashMap<>();
     static ArrayList<String> groupNameList = new ArrayList<>();
-    static ArrayList<ArrayList<Job>> parties = new ArrayList<>();
 
-    public static void hmmPut(String groupName, ArrayList<Job> party) {
-
-        parties.get(parties.size()).add(party.get(0));
-        if (party.size() > 1) {
-            parties.get(parties.size()).add(party.get(1));
-        }
-        if (party.size() > 2) {
-            parties.get(parties.size()).add(party.get(2));
-        }
-        hmm.put(groupName, parties.get(parties.size() - 1));
-        groupNameList.add(groupName);
-        System.out.println("【" + groupName + "】グループをセーブデータに保存しました");//
-    }
-
-    public static void hmmPut(String groupName, Job job1, Job job2) {
-        hmm.computeIfAbsent(groupName, k -> new ArrayList<>()).add(job1);
-        hmm.computeIfAbsent(groupName, k -> new ArrayList<>()).add(job2);
+    public static void hmmPut(String groupName, Job job1, Job job2, Job job3) {
+        hmm.computeIfAbsent(groupName,k -> new ArrayList<>()).add(job1);
+        hmm.computeIfAbsent(groupName,k -> new ArrayList<>()).add(job2);
+        hmm.computeIfAbsent(groupName,k -> new ArrayList<>()).add(job3);
         hmm.put(groupName, hmm.get(groupName));
         groupNameList.add(groupName);
         System.out.println("【" + groupName + "】グループをセーブデータに保存しました");
     }
-
+    
+    public static void hmmPut(String groupName, Job job1, Job job2) {
+        hmm.computeIfAbsent(groupName,k -> new ArrayList<>()).add(job1);
+        hmm.computeIfAbsent(groupName,k -> new ArrayList<>()).add(job2);
+        hmm.put(groupName, hmm.get(groupName));
+        groupNameList.add(groupName);
+        System.out.println("【" + groupName + "】グループをセーブデータに保存しました");
+    }
+    
     public static void hmmPut(String groupName, Job job1) {
-        hmm.computeIfAbsent(groupName, k -> new ArrayList<>()).add(job1);
+        hmm.computeIfAbsent(groupName,k -> new ArrayList<>()).add(job1);
         hmm.put(groupName, hmm.get(groupName));
         groupNameList.add(groupName);
         System.out.println("【" + groupName + "】グループをセーブデータに保存しました");
@@ -60,7 +54,7 @@ public class MemberManager {
         String gnm = 1 + ":" + groupNameList.get(0);
         for (int k = 1; k < groupNameList.size(); k++) {
             gnm += "\r\n" + (k + 1) + ":" + groupNameList.get(k);
-        }//
+        }
         return gnm;
     }
 }
