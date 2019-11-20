945a77ff8e8e3a08d6f9fbc52bae65c69ed2a83d
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-20 15:08:10 +0900

    monsterAttackの修正

--- src/mygame/Command.java
+++ src/mygame/Command.java
@@ -5,19 +5,24 @@ import java.util.concurrent.ThreadLocalRandom;
 import static mygame.Combat.*;
 
 public class Command {
-    
+
     public static void p(String msg) {
         System.out.print(msg);
     }
-    
+
     public static void pl(String msg) {
         System.out.println(msg);
     }
-    
+
     public static void monstersAttack() {
         for (int i = 0; i < monsterParty.size(); i++) {
-            int jbran = ThreadLocalRandom.current().nextInt(0, 3);
-            monsterParty.get(i).attack(combatParty.get(jbran));
+            for (;;) {
+                int jbran = ThreadLocalRandom.current().nextInt(0, 3);
+                if (combatParty.get(jbran).getHp() != 0) {
+                    monsterParty.get(i).attack(combatParty.get(jbran));
+                    break;
+                }
+            }
         }
     }
 }
