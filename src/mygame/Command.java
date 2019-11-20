825d53df8400befd0520e687a69f4472223ef959
kbc19a21 <kbc19a21@LAPTOP-9CC8QO19>
2019-11-20 16:11:19 +0900

    monsterAttackの修正(2)

--- src/mygame/Command.java
+++ src/mygame/Command.java
@@ -17,7 +17,7 @@ public class Command {
     public static void monstersAttack() {
         for (int i = 0; i < monsterParty.size(); i++) {
             for (;;) {
-                int jbran = ThreadLocalRandom.current().nextInt(0, 3);
+                int jbran = ThreadLocalRandom.current().nextInt(0, combatParty.size());
                 if (combatParty.get(jbran).getHp() != 0) {
                     monsterParty.get(i).attack(combatParty.get(jbran));
                     break;
