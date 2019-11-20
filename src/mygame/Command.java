60c9e70356945ada0660d25a3f7de3c4ca73f0e9
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-11-20 13:41:30 +0900

    コマンドクラスを追加

---
+++ src/mygame/Command.java
@@ -0,0 +1,12 @@
+package mygame;
+
+public class Command {
+
+    public static void p(String msg) {
+        System.out.print(msg);
+    }
+
+    public static void pl(String msg) {
+        System.out.println(msg);
+    }
+}
