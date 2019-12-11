a762abcca17a5039292d3bec70bc50e033d5f587
kbc19a17 <kbc19a17@LAPTOP-TNSJCQ7O>
2019-12-11 14:06:43 +0900

    実行時にセーブデータをロード

--- src/mygame/MyGame.java
+++ src/mygame/MyGame.java
@@ -6,11 +6,13 @@ import static mygame.MemberManager.*;
 import static mygame.Insert.*;
 import static mygame.PlayGame.*;
 import static mygame.Combat.*;
+import static mygame.Command.read;
 
 public class MyGame {
     //
     public static void main(String[] args) throws IOException {
         for (;;) {
+            read();
             game();
             combat();
             System.out.println("CONTINUEしますか?");
